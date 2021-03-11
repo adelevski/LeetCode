74a0ffc318cc74ffec12826c1df4438d40ee49b6
Atanas Delevski <adelevski@gmail.com>
2021-03-10 22:28:46 -0600

    added howUm

---
+++ memoization/howSum.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+def howSum(targetSum, numbers, memo={}):
+    if (targetSum in memo): return memo[targetSum]
+    if (targetSum == 0): return []
+    if (targetSum < 0): return None
+
+    for num in numbers:
+        remainder = targetSum - num
+        remainderResult = howSum(remainder, numbers)
+        if (remainderResult != None):
+            memo[targetSum] = [*remainderResult, num]
+            return memo[targetSum]
+
+    memo[targetSum] = None
+    return None
+
+
+# print(howSum(7, [2, 3]))
+# print(howSum(7, [5, 3, 4, 7]))
+print(howSum(7, [2, 4]))
+print(howSum(8, [2, 3, 5]))
+print(howSum(300, [7, 14]))
