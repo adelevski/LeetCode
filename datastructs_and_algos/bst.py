0d97de4802c9d6e652aaa6d24cc40155cc3d30cf
Atanas Delevski <adelevski@gmail.com>
2021-04-08 11:51:09 -0500

    update

---
+++ datastructs_and_algos/bst.py
@@ -0,0 +1,30 @@
+class node:
+    def __init__(self, value=None):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+    
+class binary_search_tree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, value):
+        if self.root == None:
+            self.root = node(value)
+        else:
+            self._insert(value, self.root)
+
+    def _insert(self, value, cur_node):
+        if value < cur_node.value:
+            if cur_node.left_child == None:
+                cur_node.left_child = node(value)
+            else:
+                self._insert(value, cur_node.left_child)
+        elif value > cur_node.value:
+            if cur_node.right_child == None:
+                cur_node.right_child = node(value)
+            else:
+                self._insert(value, cur_node.right_child)
+        else:
+            print("Value already in tree!")
+    
