123653aabc1c93b0a63227108ab125b131e98997
Atanas Delevski <adelevski@gmail.com>
2021-04-09 22:19:27 -0500

    new leetcode

---
+++ leetcode/buddyStrings.py
@@ -0,0 +1,21 @@
+def buddyStrings(a, b):
+    if len(a) != len(b):
+        return False
+    if a == b and len(set(a)) < len(a):
+        return True
+    differences = []
+    for x in range(len(a)):
+        if a[x] != b[x]:
+            differences.append([a[x], b[x]])
+    
+    if len(differences) == 2 and differences[0] == differences[1][::-1]:
+        return True
+
+
+
+print(buddyStrings('ab', 'ba'))
+
+# DID NOT KNOW ABOUT THIS PYTHON FUNCTIONALITY! THIS IS AMAZING!
+alist = [1, 2, 3, 4, 5]
+
+print(alist[::-1])
