d453a4f4e8adeff33d9b4d6e01ee016a35578575
Atanas Delevski <adelevski@gmail.com>
2021-04-15 13:48:28 -0500

    added twoStrings

---
+++ hackerrank/Interview Preperation Kit/Dictionaries and Hashmaps/twoStrings.py
@@ -0,0 +1,22 @@
+
+s1 = 'and'
+s2 = 'art'
+s3 = 'be'
+s4 = 'cat'
+
+def twoStrings(s1, s2):
+    first = {}
+    second = {}
+    commonChars = False
+    for char in s1:
+        if char not in first:
+            first[char] = 1
+    for char in s2:
+        if char not in second:
+            second[char] = 1
+    for char in first:
+        if char in second:
+            return 'YES'
+    return 'NO'
+
+print(twoStrings(s3, s4))
