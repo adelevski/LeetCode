b6525f69bd4778bbbf5475e207f97b66a3f95836
Atanas Delevski <adelevski@gmail.com>
2021-04-16 11:43:58 -0500

    added fib

---
+++ leetcode/(506)fibonacciNumber.py
@@ -0,0 +1,18 @@
+
+
+n = 100
+
+def fib(n):
+    if n == 0: return 0
+    if n == 1: return 1
+    prev1, prev2 = 1, 0
+    output = 0
+    for i in range(2, n+1):
+        output = prev1 + prev2
+        prev2 = prev1
+        prev1 = output
+
+    return output 
+
+
+print(fib(n))
