a99e76a8985da41a2cf13b02d7183520ae449910
Atanas Delevski <adelevski@gmail.com>
2021-04-29 13:42:23 -0500

    added unique paths 1

---
+++ leetcode/Medium/(62)uniquePaths.py
@@ -0,0 +1,16 @@
+
+m = 3
+n = 7
+
+def uniquePaths(m , n):
+    dp = [[0 for col in range(m)] for row in range(n)]
+    for i in range(m): dp[0][i] = 1
+    for i in range(n): dp[i][0] = 1
+
+    for row in range(1, n):
+        for col in range(1, m):
+            dp[row][col] = dp[row-1][col] + dp[row][col-1]
+    
+    return dp[n-1][m-1]
+
+print(uniquePaths(m, n))
