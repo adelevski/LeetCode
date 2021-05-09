c269c56e52539d807a4e92d34dff8b3f3aadf706
Atanas Delevski <adelevski@gmail.com>
2021-05-09 00:22:13 -0500

    finished min time required

--- hackerrank/Interview Preperation Kit/Search/minTimeRequired.py
+++ hackerrank/Interview Preperation Kit/Search/minTimeRequired.py
@@ -1,8 +1,8 @@
 
 
-machines = [2, 3, 2]
-goal = 10
-# Output = 8
+# machines = [2, 3, 2]
+# goal = 10
+# # Output = 8
 
 # machines = [2, 3]
 # goal = 5
@@ -12,22 +12,25 @@ goal = 10
 # goal = 10
 # # Output = 7
 
-# machines = [4, 5, 6]
-# goal = 12
-# # Output = 20
+machines = [4, 5, 6]
+goal = 12
+# Output = 20
 
 
 def minTime(machines, goal):
-    machines.sort()
-    N = len(machines)
-    current = 0
-    day = 1
-    while current < goal:
-        for i in range(N):
-            if machines[i]%day==0:
-                current += 1
-        day+=1
-    return day
+    machines, count = sorted(machines), len(machines)
+    min_day = (goal // count + 1) * machines[0]
+    max_day = (goal // count + 1) * machines[-1]
 
+    while min_day < max_day:
+        day = (max_day + min_day) // 2
+        day_sum = sum(day // m for m in machines)
+
+        if day_sum >= goal:
+            max_day = day
+        else:
+            min_day = day + 1
+
+    return min_day
 
 print(minTime(machines, goal))
