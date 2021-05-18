2f6d21ef09400c6d8b05a38d09dd7781f73c27be
Atanas Delevski <adelevski@gmail.com>
2021-05-18 01:16:29 -0500

    started abbreviation

---
+++ hackerrank/Interview Preperation Kit/Dynamic Programming/abbreviation.py
@@ -0,0 +1,24 @@
+
+# a = 'AbcDE'
+# b = 'ABDE'
+# # Output: YES
+
+# a = 'AbcDE'
+# b = 'AFDE'
+# # Output: NO
+
+# a = 'daBcd'
+# b = 'ABC'
+# # Output: YES
+
+a = 'KXzQ'
+b = 'K'
+# Output: NO
+
+def abbreviation(a, b):
+    dp = ''
+    
+
+
+
+print(abbreviation(a, b))
