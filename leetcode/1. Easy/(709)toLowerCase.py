632984b393b50d7d9ed52d36a9c399557f66d581
Atanas Delevski <adelevski@gmail.com>
2021-05-24 16:47:32 -0500

    update 709

--- leetcode/1. Easy/(709)toLowerCase.py
+++ leetcode/1. Easy/(709)toLowerCase.py
@@ -3,8 +3,13 @@
 s = "LOVELY"
 # Output: "lovely"
 
+# Works but its too easy and considered "cheating" :/
+# def toLowerCase(s):
+#     return s.lower()
+
+# ASCII implentation 
 def toLowerCase(s):
-    return s.lower()
+    return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in s)
 
 
 print(toLowerCase(s))
