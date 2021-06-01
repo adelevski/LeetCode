9c9ee53a471abc20537921b32ba15cbb49577eab
Atanas Delevski <adelevski@gmail.com>
2021-05-31 22:43:02 -0500

    added 1268

--- leetcode/2. Medium/(1268)searchSuggestionsSystem.py
+++ leetcode/2. Medium/(1268)searchSuggestionsSystem.py
@@ -1,11 +1,26 @@
 
 
-products = ["mobile","mouse","moneypot","monitor","mousepad"]
-searchWord = "mouse"
+# products = ["mobile","mouse","moneypot","monitor","mousepad"]
+# searchWord = "mouse"
+
+products = ["bags","baggage","banner","box","cloths"]
+searchWord = "bags"
+# Output: [["baggage","bags","banner"],["baggage","bags","banner"],["baggage","bags"],["bags"]]
 
 
 def suggestedProducts(products, searchWord):
-    pass
+    output = []
+    products.sort()
+
+    for i,c in enumerate(searchWord):
+        tmp = []
+        for p in products:
+            if i<len(p) and c==p[i]:
+                tmp.append(p)
+        output.append(tmp[:3])
+        products = tmp
+    return output
+
 
 
 print(suggestedProducts(products, searchWord))
