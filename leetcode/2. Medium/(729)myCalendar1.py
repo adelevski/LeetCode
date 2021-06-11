06b61c508472d31c90ce2b85b7447719203fb349
Atanas Delevski <adelevski@gmail.com>
2021-06-11 07:53:05 -0500

    finished 729

--- leetcode/2. Medium/(729)myCalendar1.py
+++ leetcode/2. Medium/(729)myCalendar1.py
@@ -1,13 +1,26 @@
 
-
+# I do not think this will run in a personal environment unless you adjust for the class
 
 class MyCalendar:
 
     def __init__(self):
-        pass
+        self.cal = [(0, 0), (10**9, 10**9)]
 
     def book(self, start: int, end: int) -> bool:
-        pass
+        l, r = 0, len(self.cal)
+        while l<r:
+            mid = (l+r)//2
+            if end == self.cal[mid][0]:
+                l = mid
+                break
+            elif end > self.cal[mid][0]:
+                l = mid + 1
+            else:
+                r = mid
+        if start < self.cal[l-1][1]: return False
+        self.cal.insert(l, (start, end))
+        return True
+            
 
 
 # Your MyCalendar object will be instantiated and called as such:
