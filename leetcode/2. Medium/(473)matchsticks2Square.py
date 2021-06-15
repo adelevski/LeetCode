87eba13080b39bfe9af85709a772d3db684d4636
Atanas Delevski <adelevski@gmail.com>
2021-06-15 12:27:03 -0500

    added 473

---
+++ leetcode/2. Medium/(473)matchsticks2Square.py
@@ -0,0 +1,38 @@
+
+matchsticks = [1,1,2,2,2]
+# Output: true
+
+# matchsticks = [3,3,3,3,4]
+# # Output: false
+
+
+def makeSquare(matchsticks):
+    N = len(matchsticks)
+
+    per = sum(matchsticks)
+
+    if per % 4 != 0: return False
+    else: side = per//4
+
+    matchsticks.sort(reverse=True)
+    def dfs(a,b,c,d,k):
+        if k==N:
+            if a==b==c==d:
+                return True
+            return False
+        m = matchsticks[k]
+
+        if a+m <= side and dfs(a+m,b,c,d,k+1):
+            return True
+        if b+m <= side and dfs(a,b+m,c,d,k+1):
+            return True
+        if c+m <= side and dfs(a,b,c+m,d,k+1):
+            return True
+        if d+m <= side and dfs(a,b,c,d+m,k+1):
+            return True
+        return False
+    
+    return dfs(0,0,0,0,0)
+
+
+print(makeSquare(matchsticks))
