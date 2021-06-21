503ee27a1c72df733b32310b06c718d00e6f556c
Atanas Delevski <adelevski@gmail.com>
2021-06-21 11:13:31 -0500

    finished 795

--- leetcode/2. Medium/(795)numSubarraysWithBoundedMax.py
+++ leetcode/2. Medium/(795)numSubarraysWithBoundedMax.py
@@ -1,5 +1,35 @@
 
+# nums = [2,1,4,3]
+# left = 2
+# right = 3
+# # Output: 3
 
 
+nums = [2,9,2,5,6]
+left = 2
+right = 8
+# Output: 7
 
-#  started today, will finish tomorrow
+
+def numSubarrays(nums,left,right):
+    l_stack,i_stack,output = 0,0,0
+    last_i = -1
+
+    for i,n in enumerate(nums):
+        if n<left:
+            if i_stack > 0:
+                output += i_stack + l_stack
+                output -= i-last_i-1
+            l_stack += 1
+        elif left<=n<=right:    
+            output += i_stack + l_stack + 1
+            i_stack += 1
+            last_i = i
+        else:
+            l_stack = 0
+            i_stack = 0
+
+
+    return output
+
+print(numSubarrays(nums, left, right))
