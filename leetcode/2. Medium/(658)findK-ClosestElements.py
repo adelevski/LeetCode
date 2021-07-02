aa40d18a1336eac91a63583b81fc749a5595b55f
adelevski <adelevski@gmail.com>
2021-07-02 18:59:49 -0500

    update

--- leetcode/2. Medium/(658)findK-ClosestElements.py
+++ leetcode/2. Medium/(658)findK-ClosestElements.py
@@ -4,18 +4,23 @@
 # x = 3
 # Output: [1,2,3,4]
 
-arr = [1,2,3,4,5]
-k = 4
-x = -1
+# arr = [1,2,3,4,5]
+# k = 4
+# x = -1
 # Output: [1,2,3,4]
+
+arr = [0,1,1,1,2,3,6,7,8,9]
+k = 9
+x = 4
 
 
 def find(arr, k, x):
     d = {}
     for i in range(len(arr)):
-        d[arr[i]] = abs(arr[i]-x)
-    # return d
-    return sorted([k for k,v in sorted(d.items(), key=lambda x: x[1])][:k])
+        d.setdefault(arr[i], [0, abs(arr[i]-x)])
+        d[arr[i]][0] += 1
+    lst = [v for k,v in d.items()]
+    return lst
         
 
 
