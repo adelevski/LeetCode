5190c00cbe9e88d8f65cd50d441a564b0ddafeb8
adelevski <adelevski@gmail.com>
2021-07-04 13:28:20 -0500

    finished 1220

--- leetcode/3. Hard/(1220)countVowelsPermutation.py
+++ leetcode/3. Hard/(1220)countVowelsPermutation.py
@@ -3,7 +3,11 @@ n = 1
 # Output: 5
 
 def countVowels(n):
-    pass
+    a = e = i = o = u = 1
+    MOD = 10**9 + 7
+    for _ in range(n-1):
+        a, e, i, o, u = (e + i + u) % MOD, (a + i) % MOD, (e + o) % MOD, i, (i + o) % MOD
+    return (a + e + i + o + u) % MOD
 
 
 
