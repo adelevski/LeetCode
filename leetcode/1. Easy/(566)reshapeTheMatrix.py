685c6fad4544fe41f9b0b6696feaeed89b64ae75
adelevski <adelevski@gmail.com>
2021-07-05 14:50:30 -0500

    finished 566

--- leetcode/1. Easy/(566)reshapeTheMatrix.py
+++ leetcode/1. Easy/(566)reshapeTheMatrix.py
@@ -5,18 +5,14 @@ c = 4
 # Output: [[1,2,3,4]]
 
 
-def matrixReshape(mat, r, c):
-    oneD = []
-    for r in range(len(mat)):
-        for c in range(len(mat[0])):
-            oneD.append(mat[r][c])
-    
-    output = []
-    for i in range(len(oneD)):
-        
-        output.append(tmpRow)
 
-    return output
+def getCell(mat):
+    for row in mat:
+        for cell in row:
+            yield cell
+def matrixReshape(mat, r, c):
+    gen = getCell(mat)
+    return [[next(gen) for _ in range(c)] for _ in range(r)] if len(mat) * len(mat[0]) == r * c else mat
 
 
 print(matrixReshape(mat, r, c))
