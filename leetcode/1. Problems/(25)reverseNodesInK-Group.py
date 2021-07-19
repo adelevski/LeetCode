46efa0120b3f7c75271fb518faec8151ec10100a
Atanas Delevski <adelevski@gmail.com>
2021-07-19 09:18:29 -0500

    added 25

---
+++ leetcode/1. Problems/(25)reverseNodesInK-Group.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+class ListNode:
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseKGroup(head, k):
+    dummy = ListNode(0)
+    dummy.next = head
+    cur, nxt, pre = dummy, dummy, dummy
+    cnt = 0
+    while cur.next:
+        cnt += 1
+        cur = cur.next
+        
+    while cnt >= k:
+        cur = new = pre.next
+        nxt = cur.next
+        for _ in range(k-1):
+            tmp = nxt.next
+            nxt.next = cur
+            cur = nxt
+            nxt = tmp
+        
+        pre.next = cur
+        new.next = nxt
+        pre = new
+        cnt -= k
+        
+    return dummy.next
