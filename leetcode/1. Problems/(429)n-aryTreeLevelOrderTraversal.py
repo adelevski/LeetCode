a2275a1d77ceeb051df1ec097864d9e17c74ffb2
Atanas Delevski <adelevski@gmail.com>
2021-08-06 21:45:12 -0500

    added 429

---
+++ leetcode/1. Problems/(429)n-aryTreeLevelOrderTraversal.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val 
+        self.children = children
+
+
+def levelOrder(root):
+    d = defaultdict(list)
+        
+    def dfs(node, level):
+        if not node: return
+        d[level].append(node.val)
+        if node.children:
+            for child in node.children:
+                dfs(child, level+1)
+    
+    dfs(root, 0)
+    
+    return d.values()
+
+# root = [1,null,3,2,4,null,5,6]
+# Output: [[1],[3,2,4],[5,6]]
+
+node = Node(1, [Node(3, [Node(5), Node(6)]), Node(2), Node(4)])
+
+print(levelOrder(node))
