379f26f03f9281027c334bafca73758ba4a902d9
Atanas Delevski <adelevski@gmail.com>
2021-09-08 14:30:50 -0500

    added 848

---
+++ leetcode/(848)shiftingLetters.py
@@ -0,0 +1,26 @@
+
+# s = "abc"
+# shifts = [3,5,9]
+# Output: "rpl"
+
+s = "aaa"
+shifts = [1,2,3]
+# Output: "gfd"
+
+
+
+
+
+def shiftingLetters(s, shifts):
+    n = len(s)
+    for i in range(n-2, -1, -1):
+        shifts[i] += shifts[i+1]
+        
+    ans = []
+    for i, c in enumerate(s):
+        idx = (ord(c) - ord('a') + shifts[i]) % 26
+        ans.append(chr(idx + ord('a')))
+    return "".join(ans)
+
+
+print(shiftingLetters(s, shifts))
