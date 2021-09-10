3f4cf3d11c1eddf4eafed422675117d47dea5a30
Atanas Delevski <adelevski@gmail.com>
2021-09-10 09:07:18 -0500

    add 446

---
+++ leetcode/(446)arithmeticSlices2.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+nums = [2,4,6,8,10]
+# Output: 7
+
+def numOfSlices(nums):
+    total = 0
+    n = len(nums)
+    dp = [Counter() for item in nums]
+    for i in range(n):
+        for j in range(i):
+            diff = nums[i] - nums[j]
+            dp[i][diff] += dp[j][diff] + 1
+            total += dp[j][diff]
+    return total
+
+print(numOfSlices(nums))
+
+
