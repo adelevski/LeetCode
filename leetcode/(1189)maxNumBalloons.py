b7928d07cf5cbaf73cc281e91e84ec61c67a180f
Atanas Delevski <adelevski@gmail.com>
2021-09-13 09:40:15 -0500

    made better

--- leetcode/(1189)maxNumBalloons.py
+++ leetcode/(1189)maxNumBalloons.py
@@ -12,14 +12,11 @@ text = 'krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrs
 # Output: 10
 
 def maxNumBalloons(text):
-    singles_dict = {'b': 0, 'a': 0, 'n': 0}
-    doubles_dict = {'l': 0, 'o': 0}
+    d = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
     n = len(text)
     for i in range(n):
-        if text[i] in singles_dict.keys():
-            singles_dict[text[i]] += 1
-        elif text[i] in doubles_dict.keys():
-            doubles_dict[text[i]] += 1
-    return min(min(singles_dict.values()), min(doubles_dict.values()) // 2)
+        if text[i] in d.keys():
+            d[text[i]] += 1
+    return min(d['b'], d['a'], d['l']//2, d['o']//2, d['n'])
 
 print(maxNumBalloons(text))
