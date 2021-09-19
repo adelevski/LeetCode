d085f5c53eb594dfe7d3f45c36d5b97766b7bb3b
Atanas Delevski <adelevski@gmail.com>
2021-09-19 14:16:49 -0500

    reorganized plus added 115

---
+++ Python/(115)distinctSubsequences.py
@@ -0,0 +1,22 @@
+s = "rabbbit"
+t = "rabbit"
+# Output: 3
+
+s = "babgbag"
+t = "bag"
+# Output: 5
+
+
+def numDistinct(s, t):
+    
+    def dp(i,j):
+        if i == -1: return j == -1
+        if j == -1: return j == -1
+        return dp(i-1, j) + (s[i] == t[j]) * dp(i-1, j-1)
+
+    return dp(len(s) - 1, len(t) - 1)
+
+
+
+
+print(numDistinct(s, t))
