217457f3fe4636c4418da7ab3df2850c604e4461
Atanas Delevski <adelevski@gmail.com>
2021-09-29 11:23:47 -0500

    added 725

---
+++ Python/(725)splitLinkedListInParts.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+      
+head = [1,2,3]
+k = 5
+# Output: [[1],[2],[3],[],[]]
+
+
+
+def splitListToparts(head, k):
+    cur = root
+    N = 0
+    while cur:
+        cur = cur.next
+        N += 1
+    d, r = divmod(N, k)
+
+    ans = []
+    cur = root
+    for i in range(k):
+        head = cur
+        for j in range(d + (i < r) - 1):
+            if cur: cur = cur.next
+        if cur:
+            cur.next, cur = None, cur.next
+        ans.append(head)
+    return ans
+
+
+
+
+print(splitListToparts(head, k))
