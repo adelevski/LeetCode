dbfce1366621f482e844e89a72b1777ae9655441
Atanas Delevski <adelevski@gmail.com>
2021-10-04 12:26:00 -0500

    added 1143

---
+++ Python/(1143)longestCommonSubsequence.py
@@ -0,0 +1,30 @@
+from itertools import product
+
+text1 = "abcde"
+text2 = "ace" 
+# Output: 3 
+
+
+text1 = "abc"
+text2 = "abc"
+# Output: 3
+
+
+def longestCommonSubsequence(text1, text2):
+    text1 = "!" + text1
+    text2 = "!" + text2
+    m, n = len(text1), len(text2)
+    dp = [[0] * n for _ in range(m)]
+    dp[0][0] = 1
+    for i, j in product(range(m), range(n)):
+        if text1[i] == text2[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    return dp[-1][-1] - 1
+
+
+
+
+print(longestCommonSubsequence(text1, text2))
