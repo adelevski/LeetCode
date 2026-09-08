754b7da4602d7348725fd1caea284225ce3f36a8
Atanas Delevski <adelevski@gmail.com>
2026-09-08 08:56:21 -0500

    pre-migration sync

---
+++ Python/(962)maxWidthRamp.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def maxWidthRamp(nums: List[int]) -> int:
+    
+    pass
+
+
+print(f"Answer: {maxWidthRamp([6,0,8,2,1,5])}. Should be 4")
+print(f"Answer: {maxWidthRamp([9,8,1,0,1,9,4,0,4,1])}. Should be 7")
